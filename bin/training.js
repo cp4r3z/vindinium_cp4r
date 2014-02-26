3d1fbffea75410a8dae147422815e071a35e4b2f
OpenShift guest <cp4r3z@gmail.com>
2014-02-25 22:44:51 -0500

    Training file

---
+++ bin/training.js
@@ -0,0 +1,10 @@
+// #!/usr/bin/node
+// # encoding: UTF-8
+
+function training() {
+  console.log('Training...');
+  //node ./759483/client.js pucddn25 training 10 http://vindinium.org
+}
+
+training();
+process.exit();
